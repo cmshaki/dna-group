137ad243d57b2a8fd2a9df81215b3a55452f3a32
Cris Shaki <cmshaki@gmail.com>
2019-10-03 21:13:06 +0300

    Added low-res video

--- src/mission/index.js
+++ src/mission/index.js
@@ -3,6 +3,6 @@ import "../assets/shared/js/cursor";
 import "../assets/shared/js/menu";
 
 // Imports video to be used
-import "../assets/shared/video/DNA_Background.mp4";
+import "../assets/shared/video/Compressed_DNA_Bkrd.mp4";
 import "../assets/shared/js/video";
 import "../assets/shared/svg/DNA_Logo_Master-02.svg";
