78cdc7aa97916ccb68264ff73e81ec6d54b4d00e
Cris Shaki <cmshaki@gmail.com>
2019-10-03 13:08:09 +0300

    Added button animations

--- src/assets/shared/js/cursor.js
+++ src/assets/shared/js/cursor.js
@@ -1,20 +1,34 @@
 const cursor = document.querySelector(".cursor");
 const cursor2 = document.querySelector(".cursor2");
-const buttons = document.getElementsByTagName("button");
+const buttons = document.querySelectorAll("button");
 
-document.addEventListener("mousemove", e => {
-  cursor.setAttribute("style", `top: ${e.pageY - 3}px; left: ${e.pageX - 3}px`);
+let initialHeight = "30px";
+let initialWidth = "30px";
+
+// Adding Event Listener for all buttons on a page
+// This will make sure that
+buttons.forEach(item => {
+  item.addEventListener("mouseover", () => {
+    initialHeight = "60px";
+    initialWidth = "60px";
+  });
+  item.addEventListener("mouseout", () => {
+    initialHeight = "30px";
+    initialWidth = "30px";
+  });
+});
+
+// The document mouse move
+document.addEventListener("mousemove", event => {
+  cursor.setAttribute(
+    "style",
+    `top: ${event.pageY - 3}px; left: ${event.pageX - 3}px`
+  );
+  const w = cursor2.getBoundingClientRect().width;
+  const h = cursor2.getBoundingClientRect().height;
   cursor2.setAttribute(
     "style",
-    `top: ${e.pageY - 15}px; left: ${e.pageX - 15}px`
+    `top: ${event.pageY - h / 2}px; left: ${event.pageX -
+      w / 2}px; height: ${initialHeight}; width: ${initialWidth}`
   );
-});
-buttons.forEach(item => {
-  item.addEventListener("mouseover", e => {
-    cursor2.setAttribute(
-      "style",
-      `top: ${e.pageY - 25}px; left: ${e.pageX -
-        25}px; width: 50px !important; height: 50px !important`
-    );
-  });
 });
