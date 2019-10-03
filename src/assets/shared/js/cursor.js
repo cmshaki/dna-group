b78be7cea51c61f75b7669d3ed10bafa5a4afeef
Cris Shaki <cmshaki@gmail.com>
2019-10-03 11:56:47 +0300

    Further text modifications

--- src/assets/shared/js/cursor.js
+++ src/assets/shared/js/cursor.js
@@ -1,5 +1,6 @@
 const cursor = document.querySelector(".cursor");
 const cursor2 = document.querySelector(".cursor2");
+const buttons = document.getElementsByTagName("button");
 
 document.addEventListener("mousemove", e => {
   cursor.setAttribute("style", `top: ${e.pageY - 3}px; left: ${e.pageX - 3}px`);
@@ -7,4 +8,13 @@ document.addEventListener("mousemove", e => {
     "style",
     `top: ${e.pageY - 15}px; left: ${e.pageX - 15}px`
   );
+});
+buttons.forEach(item => {
+  item.addEventListener("mouseover", e => {
+    cursor2.setAttribute(
+      "style",
+      `top: ${e.pageY - 25}px; left: ${e.pageX -
+        25}px; width: 50px !important; height: 50px !important`
+    );
+  });
 });
