0a5bb3362bb6439ea38e1edf4d863ec85f4d09bf
Cris Shaki <cmshaki@gmail.com>
2019-10-03 10:19:27 +0300

    Added text

--- src/assets/shared/js/cursor.js
+++ src/assets/shared/js/cursor.js
@@ -1,5 +1,6 @@
 const cursor = document.querySelector(".cursor");
 const cursor2 = document.querySelector(".cursor2");
+
 document.addEventListener("mousemove", e => {
   cursor.setAttribute("style", `top: ${e.pageY - 3}px; left: ${e.pageX - 3}px`);
   cursor2.setAttribute(
