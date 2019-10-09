9eefd387b2009011497ab0bdfa453b9bcccfda8b
Cris Shaki <cmshaki@gmail.com>
2019-10-09 14:44:40 +0300

    Minor iterations to the DNA website

--- src/connect/index.js
+++ src/connect/index.js
@@ -3,5 +3,5 @@ import "../assets/shared/js/cursor";
 import "../assets/shared/js/menu";
 
 // Imports video to be used
-import "../assets/shared/video/DNA_Background.mp4";
+import "../assets/shared/video/Contact_Video.mp4";
 //import "../assets/shared/js/video";
